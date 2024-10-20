29b5f3891329c33e5b62810eff2cb764e246fe88
KamilHanna <131144516+KamilHanna@users.noreply.github.com>
2024-10-20 21:59:47 +0200

    Fixed RungeKutta.hpp

--- include/RungeKutta.hpp
+++ include/RungeKutta.hpp
@@ -8,14 +8,14 @@ using namespace utils;
 
 
 #define alpha1 (64.0/120.0)
-#define alpha2 (30.0/120.0)
+#define alpha2 (34.0/120.0)
 #define alpha3 (50.0/120.0)
 #define alpha4 (90.0/120.0)
 
 //RHS function
 template<Addressing_T A>
 inline Real rhs(const StaggeredGrid<A> &grid, Component c, int i, int j, int k, double Kappa, int Re) {
-    return Kappa * (conv(grid, c, i, j, k) + (Kappa / Re) * lap(grid, c, i, j, k));
+    return Kappa * (-(conv(grid, c, i, j, k) + (1 / Re)) * lap(grid, c, i, j, k));
 }
 
 //Runge-Kutta method
@@ -36,9 +36,9 @@ void rungeKutta(int n, int Re, const StaggeredGrid<Addressing_T::STANDARD> grid,
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             for (int k = 0; k < n; ++k) {
-                K2(Component::U, i, j, k) = grid(Component::U, i, j, k) - rhs(grid, Component::U, i, j, k, Kappa, Re);
-                K2(Component::V, i, j, k) = grid(Component::V, i, j, k) - rhs(grid, Component::V, i, j, k, Kappa, Re);
-                K2(Component::W, i, j, k) = grid(Component::W, i, j, k) - rhs(grid, Component::W, i, j, k, Kappa, Re);
+                K2(Component::U, i, j, k) = grid(Component::U, i, j, k) + rhs(grid, Component::U, i, j, k, Kappa, Re);
+                K2(Component::V, i, j, k) = grid(Component::V, i, j, k) + rhs(grid, Component::V, i, j, k, Kappa, Re);
+                K2(Component::W, i, j, k) = grid(Component::W, i, j, k) + rhs(grid, Component::W, i, j, k, Kappa, Re);
             }
         }
     }
@@ -51,11 +51,11 @@ void rungeKutta(int n, int Re, const StaggeredGrid<Addressing_T::STANDARD> grid,
         for (int j = 0; j < n; ++j) {
             for (int k = 0; k < n; ++k) {
 
-                K3(Component::U, i, j, k) = K2(Component::U, i, j, k) - rhs(K2, Component::U, i, j, k, Kappa, Re) +
+                K3(Component::U, i, j, k) = K2(Component::U, i, j, k) + rhs(K2, Component::U, i, j, k, Kappa, Re) -
                                             rhs(K2, Component::U, i, j, k, Kappa, Re);
-                K3(Component::V, i, j, k) = K2(Component::V, i, j, k) - rhs(K2, Component::V, i, j, k, Kappa, Re) +
+                K3(Component::V, i, j, k) = K2(Component::V, i, j, k) + rhs(K2, Component::V, i, j, k, Kappa, Re) -
                                             rhs(K2, Component::V, i, j, k, Kappa, Re);
-                K3(Component::W, i, j, k) = K2(Component::W, i, j, k) - rhs(K2, Component::W, i, j, k, Kappa, Re) +
+                K3(Component::W, i, j, k) = K2(Component::W, i, j, k) + rhs(K2, Component::W, i, j, k, Kappa, Re) -
                                             rhs(K2, Component::W, i, j, k, Kappa, Re);
 
             }
@@ -71,13 +71,13 @@ void rungeKutta(int n, int Re, const StaggeredGrid<Addressing_T::STANDARD> grid,
             for (int k = 0; k < n; ++k) {
 
                 grid_out(Component::U, i, j, k) =
-                        K3(Component::U, i, j, k) - rhs(K3, Component::U, i, j, k, Kappa, Re) +
+                        K3(Component::U, i, j, k) + rhs(K3, Component::U, i, j, k, Kappa, Re) -
                         rhs(grid, Component::U, i, j, k, Kappa2, Re);
                 grid_out(Component::V, i, j, k) =
-                        K3(Component::V, i, j, k) - rhs(K3, Component::V, i, j, k, Kappa, Re) +
+                        K3(Component::V, i, j, k) + rhs(K3, Component::V, i, j, k, Kappa, Re) -
                         rhs(grid, Component::V, i, j, k, Kappa2, Re);
                 grid_out(Component::W, i, j, k) =
-                        K3(Component::W, i, j, k) - rhs(K3, Component::W, i, j, k, Kappa, Re) +
+                        K3(Component::W, i, j, k) + rhs(K3, Component::W, i, j, k, Kappa, Re) -
                         rhs(grid, Component::W, i, j, k, Kappa2, Re);
 
             }
