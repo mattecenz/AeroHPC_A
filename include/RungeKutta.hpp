d4f2a7f82f662eab46c141354611d3516b936847
KamilHanna <131144516+KamilHanna@users.noreply.github.com>
2024-10-21 11:31:36 +0200

    Update RungeKutta.hpp

--- include/RungeKutta.hpp
+++ include/RungeKutta.hpp
@@ -52,9 +52,9 @@ StaggeredGrid<double,Addressing_T::STANDARD>  &grid_out, double deltat) {
         for (int j = 0; j < n; ++j) {
             for (int k = 0; k < n; ++k) {
 
-                K3(Component::U, i,j,k) = K2(Component::U, i,j,k) + rhs(K2,Component::U,i,j,k,Kappa,Re) - rhs(K2,Component::U,i,j,k,Kappa,Re);
-                K3(Component::V, i,j,k) = K2(Component::V, i,j,k) + rhs(K2,Component::V,i,j,k,Kappa,Re) - rhs(K2,Component::V,i,j,k,Kappa,Re);
-                K3(Component::W, i,j,k) = K2(Component::W, i,j,k) + rhs(K2,Component::W,i,j,k,Kappa,Re) - rhs(K2,Component::W,i,j,k,Kappa,Re);  
+                K3(Component::U, i,j,k) = K2(Component::U, i,j,k) + rhs(K2,Component::U,i,j,k,Kappa,Re) - rhs(K2,Component::U,i,j,k,Kappa2,Re);
+                K3(Component::V, i,j,k) = K2(Component::V, i,j,k) + rhs(K2,Component::V,i,j,k,Kappa,Re) - rhs(K2,Component::V,i,j,k,Kappa2,Re);
+                K3(Component::W, i,j,k) = K2(Component::W, i,j,k) + rhs(K2,Component::W,i,j,k,Kappa,Re) - rhs(K2,Component::W,i,j,k,Kappa2,Re);  
 
                  
             }
