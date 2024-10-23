ef5ed4920a159492c947c2b83e3bf4fffeb976f5
KamilHanna <kamil.hanna@mail.polimi.it>
2024-10-23 18:59:22 +0000

    Fixed Errors

---
+++ build/CMakeFiles/AeroHPC_A.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/main.cpp" "CMakeFiles/AeroHPC_A.dir/main.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/main.cpp.o.d"
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/src/ForcingTerm.cpp" "CMakeFiles/AeroHPC_A.dir/src/ForcingTerm.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/src/ForcingTerm.cpp.o.d"
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/src/GhostedSG.cpp" "CMakeFiles/AeroHPC_A.dir/src/GhostedSG.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/src/GhostedSG.cpp.o.d"
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/src/Model.cpp" "CMakeFiles/AeroHPC_A.dir/src/Model.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/src/Model.cpp.o.d"
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/src/StaggeredGrid.cpp" "CMakeFiles/AeroHPC_A.dir/src/StaggeredGrid.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/src/StaggeredGrid.cpp.o.d"
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/src/error.cpp" "CMakeFiles/AeroHPC_A.dir/src/error.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/src/error.cpp.o.d"
+  "/home/viken/Desktop/AEROSPACE/AeroHPC_A/src/utils.cpp" "CMakeFiles/AeroHPC_A.dir/src/utils.cpp.o" "gcc" "CMakeFiles/AeroHPC_A.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
