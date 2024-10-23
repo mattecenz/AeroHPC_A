94cf71d24ededbaea78c170a92498c94b39dc105
KamilHanna <131144516+KamilHanna@users.noreply.github.com>
2024-10-23 13:29:58 +0200

    Update ForcingTerm.hpp

--- include/ForcingTerm.hpp
+++ include/ForcingTerm.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <cmath>  
+#include <StaggeredGrid.hpp>
 
 using namespace std;
 
@@ -11,31 +12,30 @@ class ForcingTerm {
 public:
 
     // Constructor 
-    ForcingTerm(int Re, double time = 0.0);
+    ForcingTerm(Real Re, double time = 0.0);
 
     //setters
     void set_time(double time);
-    void set_Re(int Re);
+    void set_Re(Real Re);
 
     //getters
-    int get_Re() const;
+    Real get_Re() const;
     double get_time() const;
 
+    //Time update
+    void ForcingTerm::update_time(double extraTime);
+
     // Method to compute the forcing term at a given point in space and time
-    array<double,3> compute(double x, double y, double z) const;
+    double compute(double x, double y, double z, Component c) const;
 
     // Hfunctions to compute the forcing term in each direction.
-    double computeGx(double x, double y, double z) const;
-    double computeGy(double x, double y, double z) const;
-    double computeGz(double x, double y, double z) const;
+    double computeGx(double x, double y, double z, double t, Real Re) const;
+    double computeGy(double x, double y, double z, double t, Real Re) const;
+    double computeGz(double x, double y, double z, double t, Real Re) const;
 
 private:
     double time;
-    int Re;
+    Real Re;
 };
 
 #endif // FORCING_TERM_HPP
-
-
-
-
