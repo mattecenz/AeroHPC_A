d0229d5f378f560b58c70564c80b70d962f1a352
yektaKamane <ykamaneh@gmail.com>
2024-10-26 15:35:12 +0000

    convergence

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 #include "L2NormCalculator.hpp"
 #include "StaggeredGrid.hpp"
 #include "GhostedSG.hpp"
@@ -7,17 +9,17 @@
 #include "RungeKutta.hpp"
 #include "VTKConverter.hpp"
 
-void testSolver() {
+Real testSolver(Real deltaT, index_t dim) {
 
     std::cout << "Running the solver" << std::endl;
 
     // define T & deltaT  & Re
-    const Real T = 1.;
-    const Real deltaT = 0.001;
+    const Real T = 0.1;
+    // const Real deltaT = 0.001;
     const Real Re = 4700.0;
 
     // Define dim as side dimension of the grid (just for simplicity)
-    const index_t dim = 50;
+    // const index_t dim = 50;
 
     // Define number of nodes for each axis
     const index_t nx = dim;
@@ -47,8 +49,6 @@ void testSolver() {
     Vector spacing = {sx, sy, sz};
 
     // define the mesh:
-    // instantiate from ghosted stagg grid
-    // hint: second method, num of nodes in each direction, number of ghosts=1
     GhostedSG<STANDARD> sg({nx, ny, nz}, 1);
     cout << "Grid created" << std::endl;
 
@@ -67,36 +67,6 @@ void testSolver() {
         const Real sdx = model.dx/2;
         const Real sdy = model.dy/2;
         const Real sdz = model.dz/2;
-
-        // Yes but when I have x=0 I need to do two things:
-        // 1) set only the ghosted x points by interpolating
-        // 2) set the face yz to the corresponding values
-
-        // To understand what happens we can visualize it
-        //       ---x---p---x---p---x              -> x
-        // Index:  -1   0   0   1   1
-        //              ^
-        //           boundary
-
-        // We want x[0] to be calculated (as that is inside our mesh so needs to be passed)
-        // Because we do not know it unless we can approximate it as an int
-
-        // We can do everything in a for loop for the x variable
-        /*
-        for (index_t j = -1; j < grid.ny + 1; j++){
-            for (index_t k = -1; k < grid.nz + 1; k++) {
-                Real x = -1                   * model.dx;
-                Real y = static_cast<Real>(j) * model.dy;
-                Real z = static_cast<Real>(k) * model.dz;
-
-                grid(U, -1, j, k) = 2 * ExactSolution<STANDARD>::u(0., y, z, currentTime) - grid(U, 0, j,k);
-                // These 2 are fine because we do not care about it
-                grid(V, 0, j, k) = ExactSolution<STANDARD>::v(x, y + sdy, z, currentTime);
-                grid(W, 0, j, k) = ExactSolution<STANDARD>::w(x, y, z + sdz, currentTime);
-            }
-        }
-        // But the only tricky part is that we will need to calculate also x=0 in the rk
-        */
 
         for (index_t i = -1; i < 1; i++)
             for (index_t j = -1; j < grid.ny + 1; j++)
@@ -185,6 +155,7 @@ void testSolver() {
 
     model.applyBCs(); // for T=0
 
+    Real l2Norm = 0.0;
     while (currentTime < T) {
         // call RK (obtain model at currentTime + dt)
         rungeKutta(model, Y2, Y3, deltaT, currentTime);
@@ -194,19 +165,56 @@ void testSolver() {
 
         model.applyBCs(); // for T= currTime + dt
 
-        /* TODO ??At this point we should have the solution on all the domain at time = currTime + dt??
-             (so we can compare with exact solution at time = currTime + dt) */
-        Real l2Norm = computeL2Norm<STANDARD>(model, currentTime);
-        printf("%5d) t %0.4f l2 %f\n", stepCounter, currentTime, l2Norm);
+        if(currentTime >= T){ 
+            l2Norm = computeL2Norm<STANDARD>(model, currentTime);
+            printf("%5d) t %0.4f l2 %f\n", stepCounter, currentTime, l2Norm);
+        }
+        // l2Norm = computeL2Norm<STANDARD>(model, currentTime);
+        // printf("%5d) t %0.4f l2 %f\n", stepCounter, currentTime, l2Norm);
     }
 
     // Output of last iteration
-    VTKFile file = VTKConverter::exportModel(model, "testsolver output of last time iteration");
-    file.writeFile("testsolver.vtk");
+    // VTKFile file = VTKConverter::exportModel(model, "testsolver output of last time iteration");
+    // file.writeFile("testsolver.vtk");
+
+    return l2Norm;
 }
 
+
+
 int main() {
-    // try the solver
-    testSolver();
+
+    // dividing the timestep size to half
+    std::vector<Real> deltaTs = {0.0001, 0.0005, 0.00025};
+    std::vector<index_t> dims = {4, 8, 16, 32, 64};
+
+    std::vector<Real> error;
+    
+    // wrt deltaT
+    // for (size_t i=0; i<deltaTs.size(); ++i){
+    //     Real deltaT = deltaTs[i];
+    //     index_t dim = dims[0];
+    //     error.push_back(testSolver(deltaT, dim));
+    // }
+
+
+    // wrt dim
+    for (size_t i=0; i<dims.size(); ++i){
+        Real deltaT = deltaTs[0]; // first
+        index_t dim = dims[i];
+        testSolver(deltaT, dim);
+    }
+
+
+    // // wrt both
+    // for (size_t i=0; i<dims.size(); ++i){
+    //     Real deltaT = deltaT[i];
+    //     index_t dim = dims[i];
+    //     testSolver(deltaT, dim);
+    // }
+
+    for (size_t i=0; i<error.size(); ++i) std::cout << "err: " << error[i] << std::endl; 
+    std::ofstream csvFile("output.csv");
+
     return 0;
 }
