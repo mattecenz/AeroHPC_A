cd419de843654c76e70cbbd48c6f6f5a1ae516ef
yektaKamane <ykamaneh@gmail.com>
2024-11-30 20:25:29 +0000

    Neumann case finish

--- fft_test/N_with_fft.cpp
+++ fft_test/N_with_fft.cpp
@@ -42,6 +42,7 @@ int main(int argc, char *argv[]){
     const double L = 1.0;  // Length of each dimension
     const double dx = L / N;
 
+    // This b is the right-hand-side vector in Ax=b
     double *b = (double *)malloc(sizeof(double) * N * N * N);
         
     for (int i = 0; i < N; ++i) {
@@ -53,7 +54,6 @@ int main(int argc, char *argv[]){
                 // Compute the 1D linear index for the 3D grid
                 int index = i * (N * N) + j * N + k;
                 b[index] = sin(2 * M_PI * xi) * sin(2 * M_PI * yj) * sin(2 * M_PI * zk);
-
             }
         }
     }
@@ -61,16 +61,12 @@ int main(int argc, char *argv[]){
     int pRow = 0, pCol = 0;
     bool periodicBC[3] = {true, true, true};
 
-    auto start_pre = std::chrono::high_resolution_clock::now();
     if(!mpiRank) cout << "initializing " << endl;
     C2Decomp *c2d;
     c2d = new C2Decomp(N, N, N, pRow, pCol, periodicBC);
     if(!mpiRank) cout << "done initializing " << endl;
-    auto end_pre = std::chrono::high_resolution_clock::now();
 
     if(!mpiRank) cout << "Grid dim: " << N << endl;
-
-    auto start = std::chrono::high_resolution_clock::now();
 
     double xSize[3], ySize[3], zSize[3];
     xSize[0] = c2d->xSize[0];
@@ -105,27 +101,23 @@ int main(int argc, char *argv[]){
     }
 
     // Perform FFT along the X direction (on u1)
-    fftw_complex *fft_input, *fft_output;
-    fft_input = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * xSize[0] * xSize[1] * xSize[2]);
-    fft_output = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * xSize[0] * xSize[1] * xSize[2]);
-
-    // Plan for FFT along x-direction
-    fftw_plan plan_x = fftw_plan_r2r_1d(xSize[0], fft_input, fft_output, FFTW_REDFT10, FFTW_ESTIMATE);
+    double *fft_input, *fft_output;
+    fft_input = (double*)fftw_malloc(sizeof(double) * xSize[0] * xSize[1] * xSize[2]);
+    fft_output = (double*)fftw_malloc(sizeof(double) * xSize[0] * xSize[1] * xSize[2]);
 
     // Perform FFT along x (for each y and z slice)
     for (int jp = 0; jp < xSize[1]; ++jp) {
         for (int kp = 0; kp < xSize[2]; ++kp) {
             for (int ip = 0; ip < xSize[0]; ++ip) {
                 int ii = kp * xSize[1] * xSize[0] + jp * xSize[0] + ip;
-                fft_input[ip][0] = u1[ii]; // Real part
-                fft_input[ip][1] = 0;     // Imaginary part (initially 0)
+                fft_input[ip] = u1[ii];
             }
-            fftw_execute(plan_x);  // Execute FFT on each slice
+            fftw_execute(fftw_plan_r2r_1d(xSize[0], fft_input, fft_output, FFTW_REDFT10, FFTW_ESTIMATE));  // Execute FFT on each slice
 
             // Store the FFT result back into u1 (you may store real and imaginary parts here)
             for (int ip = 0; ip < xSize[0]; ++ip) {
                 int ii = kp * xSize[1] * xSize[0] + jp * xSize[0] + ip;
-                u1[ii] = fft_output[ip][0]; // Storing the real part
+                u1[ii] = fft_output[ip];
             }
         }
     }
@@ -134,27 +126,23 @@ int main(int argc, char *argv[]){
     MPI_Barrier(MPI_COMM_WORLD);
 
     // Perform FFT along the Y direction (on u2)
-    fftw_complex *fft_input_y, *fft_output_y;
-    fft_input_y = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ySize[0] * ySize[1] * ySize[2]);
-    fft_output_y = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ySize[0] * ySize[1] * ySize[2]);
-
-    // Plan for FFT along y-direction
-    fftw_plan plan_y = fftw_plan_r2r_1d(ySize[0], fft_input_y, fft_output_y, FFTW_REDFT10, FFTW_ESTIMATE);
+    double *fft_input_y, *fft_output_y;
+    fft_input_y = (double*)fftw_malloc(sizeof(double) * ySize[0] * ySize[1] * ySize[2]);
+    fft_output_y = (double*)fftw_malloc(sizeof(double) * ySize[0] * ySize[1] * ySize[2]);
 
     // Perform FFT along y (for each x and z slice)
     for (int kp = 0; kp < ySize[2]; ++kp) {
         for (int ip = 0; ip < ySize[0]; ++ip) {
             for (int jp = 0; jp < ySize[1]; ++jp) {
                 int ii = kp * ySize[1] * ySize[0] + jp * ySize[0] + ip;
-                fft_input_y[ip][0] = u2[ii]; // Real part
-                fft_input_y[ip][1] = 0;     // Imaginary part
+                fft_input_y[ip] = u2[ii];
             }
-            fftw_execute(plan_y);  // Execute FFT on each slice
+            fftw_execute(fftw_plan_r2r_1d(ySize[0], fft_input_y, fft_output_y, FFTW_REDFT10, FFTW_ESTIMATE));  // Execute FFT on each slice
 
             // Store the FFT result back into u2
             for (int jp = 0; jp < ySize[1]; ++jp) {
                 int ii = kp * ySize[1] * ySize[0] + jp * ySize[0] + ip;
-                u2[ii] = fft_output_y[ip][0]; // Storing real part
+                u2[ii] = fft_output_y[ip];
             }
         }
     }
@@ -164,34 +152,102 @@ int main(int argc, char *argv[]){
     MPI_Barrier(MPI_COMM_WORLD);
 
     // Perform FFT along the Z direction (on u3)
-    fftw_complex *fft_input_z, *fft_output_z;
-    fft_input_z = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * zSize[0] * zSize[1] * zSize[2]);
-    fft_output_z = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * zSize[0] * zSize[1] * zSize[2]);
-
-    // Plan for FFT along z-direction
-    fftw_plan plan_z = fftw_plan_r2r_1d(zSize[0], fft_input_z, fft_output_z, FFTW_REDFT10, FFTW_ESTIMATE);
+    double *fft_input_z, *fft_output_z;
+    fft_input_z = (double*)fftw_malloc(sizeof(double) * zSize[0] * zSize[1] * zSize[2]);
+    fft_output_z = (double*)fftw_malloc(sizeof(double) * zSize[0] * zSize[1] * zSize[2]);
 
     // Perform FFT along z (for each x and y slice)
     for (int jp = 0; jp < zSize[1]; ++jp) {
         for (int ip = 0; ip < zSize[0]; ++ip) {
             for (int kp = 0; kp < zSize[2]; ++kp) {
                 int ii = kp * zSize[1] * zSize[0] + jp * zSize[0] + ip;
-                fft_input_z[ip][0] = u3[ii]; // Real part
-                fft_input_z[ip][1] = 0;     // Imaginary part
+                fft_input_z[ip] = u3[ii];
             }
-            fftw_execute(plan_z);  // Execute FFT on each slice
+            fftw_execute(fftw_plan_r2r_1d(zSize[0], fft_input_z, fft_output_z, FFTW_REDFT10, FFTW_ESTIMATE));  // Execute FFT on each slice
 
             // Store the FFT result back into u3
             for (int kp = 0; kp < zSize[0]; ++kp) {
                 int ii = kp * zSize[1] * zSize[0] + jp * zSize[0] + ip;
-                u3[ii] = fft_output_z[ip][0]; // Storing the real part
+                u3[ii] = fft_output_z[ip];
             }
         }
     }
 
-    // Transpose u3 to u2 (reverse z -> y)
+    // Compute eigenvalues and divide FFT result by eigenvalues in parallel
+    for (int kp = 0; kp < zSize[2]; ++kp) {
+        int kz = ((c2d->zStart[2] + kp) < N / 2) ? (c2d->zStart[2] + kp) : (c2d->zStart[2] + kp - N);
+        for (int jp = 0; jp < zSize[1]; ++jp) {
+            int ky = ((c2d->zStart[1] + jp) < N / 2) ? (c2d->zStart[1] + jp) : (c2d->zStart[1] + jp - N);
+            for (int ip = 0; ip < zSize[0]; ++ip) {
+                int kx = ((c2d->zStart[0] + ip) < N / 2) ? (c2d->zStart[0] + ip) : (c2d->zStart[0] + ip - N);
+                
+                size_t idx = kp * zSize[1] * zSize[0] + jp * zSize[0] + ip;
+                double ksq = -(kx * kx + ky * ky + kz * kz);
+                double eigenvalue = (ksq == 0) ? 1e-10 : ksq;  // Avoid division by zero
+                u3[idx] /= eigenvalue;
+            }
+        }
+    }
+
+    // Perform inverse FFT along Z direction (on u3)
+    for (int jp = 0; jp < zSize[1]; ++jp) {
+        for (int ip = 0; ip < zSize[0]; ++ip) {
+            for (int kp = 0; kp < zSize[2]; ++kp) {
+                int ii = kp * zSize[1] * zSize[0] + jp * zSize[0] + ip;
+                fft_input_z[ip] = u3[ii];
+            }
+            // Execute inverse FFT
+            fftw_execute(fftw_plan_r2r_1d(zSize[0], fft_input_z, fft_output_z, FFTW_REDFT01, FFTW_ESTIMATE));
+
+            // Store the inverse FFT result back into u3
+            for (int kp = 0; kp < zSize[0]; ++kp) {
+                int ii = kp * zSize[1] * zSize[0] + jp * zSize[0] + ip;
+                u3[ii] = fft_output_z[ip];
+            }
+        }
+    }
+
     c2d->transposeZ2Y(u3, u2);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Perform inverse FFT along Y direction (on u2)
+    for (int kp = 0; kp < ySize[2]; ++kp) {
+        for (int ip = 0; ip < ySize[0]; ++ip) {
+            for (int jp = 0; jp < ySize[1]; ++jp) {
+                int ii = kp * ySize[1] * ySize[0] + jp * ySize[0] + ip;
+                fft_input_y[ip] = u2[ii];
+            }
+            // Execute inverse FFT
+            fftw_execute(fftw_plan_r2r_1d(ySize[0], fft_input_y, fft_output_y, FFTW_REDFT01, FFTW_ESTIMATE));
+
+            // Store the inverse FFT result back into u2
+            for (int jp = 0; jp < ySize[1]; ++jp) {
+                int ii = kp * ySize[1] * ySize[0] + jp * ySize[0] + ip;
+                u2[ii] = fft_output_y[ip];
+            }
+        }
+    }
+
     c2d->transposeY2X(u2, u1);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Perform inverse FFT along X direction (on u1)
+    for (int jp = 0; jp < xSize[1]; ++jp) {
+        for (int kp = 0; kp < xSize[2]; ++kp) {
+            for (int ip = 0; ip < xSize[0]; ++ip) {
+                int ii = kp * xSize[1] * xSize[0] + jp * xSize[0] + ip;
+                fft_input[ip] = u1[ii];
+            }
+            // Execute inverse FFT
+            fftw_execute(fftw_plan_r2r_1d(xSize[0], fft_input, fft_output, FFTW_REDFT01, FFTW_ESTIMATE));
+
+            // Store the inverse FFT result back into u1
+            for (int ip = 0; ip < xSize[0]; ++ip) {
+                int ii = kp * xSize[1] * xSize[0] + jp * xSize[0] + ip;
+                u1[ii] = fft_output[ip];
+            }
+        }
+    }    
 
 
     fftw_free(fft_input);
